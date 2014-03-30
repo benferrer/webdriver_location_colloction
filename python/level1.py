742ed274d4b90511176a3aa657c5ff6b509873c0
watircn <watircn@gmail.com>
2014-03-30 15:36:49 +0800

    add disabled

--- python/level1.py
+++ python/level1.py
@@ -9,7 +9,7 @@ file_path =  'file:///' + os.path.abspath('locator.html')
 dr.get(file_path)
 
 input_string = 'easonhan.info'
-
+'''
 # locate id=name
 id_element = dr.find_element_by_id('name')
 id_element.send_keys(input_string)
@@ -49,14 +49,18 @@ assert css_element.get_attribute('value') == input_string
 xpath_element = dr.find_element_by_xpath('//input[@id="for-xpath"]')
 xpath_element.send_keys(input_string)
 assert xpath_element.get_attribute('value') == input_string
+'''
 
 # match id ~= ^for inputs and input something
 inputs = dr.find_elements_by_tag_name('input')
+# disable one input that id leading with for
+disable_js = '$("#for-css").attr("disabled", "disabled");';
+dr.execute_script(disable_js)
 leading_str = 'Leading with for'
 for ip in inputs:
 	if re.search('^for.*', ip.get_attribute('id')): 
-		if ip.get_attribute('type') == 'text' :
+		if ip.get_attribute('type') == 'text' and ip.get_attribute('disabled') is None :
 			ip.clear()
 			ip.send_keys(leading_str)
 assert dr.find_element_by_id('for-xpath').get_attribute('value') == leading_str
-assert dr.find_element_by_id('for-css').get_attribute('value') == leading_str
+assert dr.find_element_by_id('for-css').get_attribute('disabled') == 'true'
